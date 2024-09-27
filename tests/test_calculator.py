a654f2d18310a5d92a9fa6c2e09e9a275d4617c9
cl78 <cl78@njit.edu>
2024-09-27 18:10:11 -0400

    added multiply and division and their tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,3 +8,11 @@ def test_addition():
 def test_subtraction():
     '''Test that addition function works '''    
     assert subtract(2,2) == 0
+
+def test_multiplication():
+    '''Test that multiplication works '''
+    assert multiply(2,2) == 4
+
+def test_division():
+    '''Test that division works '''
+    assert divide(2,2) == 1
